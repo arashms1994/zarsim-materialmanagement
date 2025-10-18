36c1247f930c97b94c0fcdfbb0faacd90bf3628b
arashms1994 <arash.ms1994@gmail.com>
2025-10-18 09:53:43 +0330

    Initilize

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  base: "/SitePages/materialmanagement.aspx",
+  build: {
+    outDir: "dist",
+    assetsDir: ".",
+    minify: false,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: `index.js`,
+        chunkFileNames: `chunk.js`,
+        assetFileNames: `index.css`,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [react(), tailwindcss()],
+});
