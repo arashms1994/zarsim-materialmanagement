b6dc795eee06f093f56c166a0e8b7b453689a90c
arashms1994 <arash.ms1994@gmail.com>
2025-10-18 15:58:05 +0330

    faet: add shadcn components for form

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,5 +1,12 @@
+import type DateObject from "react-date-object";
+
 export interface ITabPanelProps {
   children?: React.ReactNode;
   index: number;
   value: number;
+}
+
+export interface IPersianDatePickerProps {
+  value: string | Date | DateObject | null;
+  onChange: (date: string) => void;
 }
